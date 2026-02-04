1149aafbc2db36e6daf77f23653d1c5a6bd10610
JDEXPLOIT <100084237+JdExploit@users.noreply.github.com>
2026-02-05 00:44:48 +0100

    Delete compile_server.sh

--- compile_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "[*] Compiling C2 Server..."
-g++ -std=c++11 -pthread -o c2_server server.cpp
-if [ $? -eq 0 ]; then
-    echo "[+] Server compiled successfully!"
-    echo "[*] Run: ./c2_server"
-else
-    echo "[-] Compilation failed!"
-fi
